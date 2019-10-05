ede2553d9c3f6060020fa330c8a19468414bde16
Jury Verrigni <jury.verrigni@gmail.com>
2019-10-05 19:57:39 +0100

    Whopsie

--- build-switch.sh
+++ build-switch.sh
@@ -3,5 +3,5 @@ source /opt/devkitpro/switchvars.sh
 CPUS=$((`nproc --all` + 1))
 rm build -rf && mkdir build && cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=/opt/devkitpro/switch.cmake -DCMAKE_INSTALL_PREFIX=/opt/devkitpro/devkitA64/aarch64-none-elf/ -DLUA_USE_DLOPEN=OFF ..
-make -j`$CPUS`
+make -j`echo $CPUS`
 make install
