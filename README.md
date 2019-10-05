d6f935de37ea1af9f270cf289f307e104fe4e66e
Jury Verrigni <jury.verrigni@gmail.com>
2019-10-05 18:00:27 +0100

    Add build script

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+liblua-switch
+==================
+
+Just a script to wraps liblua build on devkitpro toolchain for switch. Just run build-switch.sh and you should have liblua.a installed into devkitpro's root ready to be linked against.
+
+==================
 
 This is Lua 5.3.2, released on 25 Nov 2015.
 
---
+++ build-switch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /opt/devkitpro/switchvars.sh
+CPUS=$((`nproc --all` + 1))
+rm build -rf && mkdir build && cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=/opt/devkitpro/switch.cmake -DCMAKE_INSTALL_PREFIX=/opt/devkitpro/devkitA64/aarch64-none-elf/ -DLUA_USE_DLOPEN=OFF ..
+make -j`$CPUS`
+make install
